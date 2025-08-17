d457ad0bdff988292a17aabb7b671c3b359a119e
kamalyanaram <armcrytube@gmail.com>
2025-08-17 23:17:14 +0400

    Dusabling buttons, isValid dirty

--- src/components/YoutubeForm.jsx
+++ src/components/YoutubeForm.jsx
@@ -31,56 +31,59 @@ function YoutubeForm() {
       initialValues={initialValues}
       onSubmit={onSubmit}
       validationSchema={validationSchema}
+      // validateOnMount
     >
-      <Form>
-        <label htmlFor="name">Name</label>
-        <Field type="text" id="name" name="name" />
-        <ErrorMessage name="name" component={TextError}/>
-        <br />
+      {(formik) => {
+        return (
+          <Form>
+            <label htmlFor="name">Name</label>
+            <Field type="text" id="name" name="name" />
+            <ErrorMessage name="name" component={TextError} />
+            <br />
 
-        <label htmlFor="email">E-mail</label>
-        <Field type="text" id="email" name="email" />
-        <ErrorMessage name="email">
-          {
-            (error) => <div className="error">{error}</div>
-          }
-        </ErrorMessage>
-        <br />
+            <label htmlFor="email">E-mail</label>
+            <Field type="text" id="email" name="email" />
+            <ErrorMessage name="email">
+              {(error) => <div className="error">{error}</div>}
+            </ErrorMessage>
+            <br />
 
-        <label htmlFor="channel">Channel</label>
-        <Field type="text" id="channel" name="channel" />
-        <ErrorMessage name="channel" />
-        <br />
+            <label htmlFor="channel">Channel</label>
+            <Field type="text" id="channel" name="channel" />
+            <ErrorMessage name="channel" />
+            <br />
 
-        <label htmlFor="comments">Comments</label>
-        <Field as="textarea" type="text" id="comments" name="comments" />
-        <br />
+            <label htmlFor="comments">Comments</label>
+            <Field as="textarea" type="text" id="comments" name="comments" />
+            <br />
 
-        <label htmlFor="address">Address</label>
-        <Field type="text" id="address" name="address">
-         {
-          (props) => {
-            const { field, form, meta } = props;
-            return (
-              <div>
-                <input id='address' type="text" {...field} />
-                {meta.touched && meta.error ? <div className="error">{meta.error}</div> : null}
-              </div>
-            );
-          }
-         }
-        </Field>
-        <br />
+            <label htmlFor="address">Address</label>
+            <Field type="text" id="address" name="address">
+              {(props) => {
+                const { field, form, meta } = props;
+                return (
+                  <div>
+                    <input id="address" type="text" {...field} />
+                    {meta.touched && meta.error ? (
+                      <div className="error">{meta.error}</div>
+                    ) : null}
+                  </div>
+                );
+              }}
+            </Field>
+            <br />
 
-        <label htmlFor="facebook">Facebook profile</label>
-        <Field type="text" id="facebook" name="social.facebook" />
-        <br />
-        <label htmlFor="twitter">Twitter profile</label>
-        <Field type="text" id="twitter" name="social.twitter" />
-        <br />
+            <label htmlFor="facebook">Facebook profile</label>
+            <Field type="text" id="facebook" name="social.facebook" />
+            <br />
+            <label htmlFor="twitter">Twitter profile</label>
+            <Field type="text" id="twitter" name="social.twitter" />
+            <br />
 
-        <button type="submit">Submit</button>
-      </Form>
+            <button type="submit" disabled={(!formik.dirty && !formik.isValid)}>Submit</button>
+          </Form>
+        );
+      }}
     </Formik>
   );
 }
